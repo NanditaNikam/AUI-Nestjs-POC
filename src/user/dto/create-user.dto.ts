b7235e2da578188aff79c15cf2faff9501eb3ed7
Nandita-Nikam <nandita.nikam@syngenta.com>
2024-06-25 19:57:20 +0530

    minor changes

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,19 +1,14 @@
-import { Field, InputType } from '@nestjs/graphql';
 import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
-
-@InputType()
 export class CreateUserDto {
-  @Field()
+ 
   @IsNotEmpty()
   @IsString()
   firstName: string;
 
-  @Field()
   @IsNotEmpty()
   @IsString()
   lastName: string;
 
-  @Field()
   @IsNotEmpty()
   @IsNumber()
   age: number;
