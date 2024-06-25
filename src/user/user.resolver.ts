3eac25dbaaddbb024e534b7f888e81b04d28e9b4
Nandita-Nikam <nandita.nikam@syngenta.com>
2024-06-25 16:36:19 +0530

    added query and mutations of graphql

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -1,13 +1,27 @@
-import { Query, Resolver, Mutation, Args } from '@nestjs/graphql';
-import { User } from './user.schema';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { User } from './entities/user.entity';
 import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Resolver(() => User)
 export class UserResolver {
   constructor(private readonly userService: UserService) {}
 
-  @Query(() => [User])
+  @Query(() => [User],)
   async users(): Promise<User[]> {
     return this.userService.findAll();
   }
+  @Mutation(() => User)
+  createUser(
+    @Args('firstName') firstName: string,
+    @Args('lastName') lastName: string,
+    @Args('age') age: number,
+  ){
+    const createUserDto = new CreateUserDto();
+    createUserDto.firstName = firstName;
+    createUserDto.lastName = lastName;
+    createUserDto.age = age;
+    return this.userService.create(createUserDto);
+  }
+
 }
